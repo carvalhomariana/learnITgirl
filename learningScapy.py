03d1e0652607319573032af854a88fa8bee657fc
Mariana Carvalho <ma.pcarvalho2@gmail.com>
2018-01-24 22:09:59 -0600

    updating

--- learningScapy.py
+++ learningScapy.py
@@ -20,11 +20,11 @@ from scapy.all import wrpcap
 # identify possible network attacks. Wireshark captures packets and display them
 # in a easy way to visualize, and filter them.
 
-# First, let's open Wireshark and record some traffic. CLick on the blue icon on
+# First, let's open Wireshark and record some traffic. Click on the blue icon on
 # the right top of the page. Open any website of your preference. Play around.
 # I use the CNN website (http://www.cnn.com) to record my own traffic in this website.
 # Click on the red square on the top left, and stop the network traffic recording.
-# Save the file with a .pcap extension. My
+# Save the file with a .pcap extension.
 # My file name is cnn-test.pcap
 
 # Let's read the pcap file  
@@ -42,7 +42,7 @@ pkts_list[245].show()
 # Let's display the IPv6 layer of source and destionation
 print ("This is the IPv6 source: ", pkts_list[245]['IPv6'].src)
 print ("This is the IPv6 destination: ", pkts_list[245]['IPv6'].dst)
-print ("This is the sport", pkts_list[245]['IPv6'].sport)
+print ("This is the sport", pkts_list[245]['TCP'].sport)
 
 # With scapy, we can also modify the IP:
 ### pkts_list[245]['IPv6'].src = "10.0.0.2"
@@ -50,7 +50,7 @@ print ("This is the sport", pkts_list[245]['IPv6'].sport)
 # We can also write the modified packets into another pcapfile
 ## wrpcap("modified.pcap", pkts_list)
 
-
+#Let's read the pcapfile for an ev
 
 
 
